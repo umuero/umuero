0fc9e94b785c6c3ed6c41a209dca2bc83e651a02
umuero <umut.erogul@somera.com.tr>
2022-02-09 17:12:15 +0300

    ig onStop fix

--- igFollow.user.js
+++ igFollow.user.js
@@ -45,8 +45,8 @@ const menu = $(`
 </ul>
 `);
 
-const progressBar = dialog.find('.crawler-progressbar');
-const progressBarLabel = progressBar.find('.crawler-progressbar-label');
+const progressBar = dialog.find(".crawler-progressbar");
+const progressBarLabel = progressBar.find(".crawler-progressbar-label");
 
 const maxFollowersPerAccount = 100000;
 const followersPerPage = 2500;
@@ -56,151 +56,171 @@ let authorId = "";
 
 let results = [];
 let idMap = {};
-let followers = []
+let followers = [];
 let pageInfo;
 let media;
 
 function addStyles() {
-    console.log('adding additional styles');
-    GM_addStyle(styleSheet);
-    var jqUI_CssSrc = GM_getResourceText('jqUI_CSS');
-    GM_addStyle(jqUI_CssSrc);
+  console.log("adding additional styles");
+  GM_addStyle(styleSheet);
+  var jqUI_CssSrc = GM_getResourceText("jqUI_CSS");
+  GM_addStyle(jqUI_CssSrc);
 }
 
 function addMenu() {
-    $('body').append(menu);
-    menu.menu();
-    menu.menu({
-        select: onMenuSelected
-    });
+  $("body").append(menu);
+  menu.menu();
+  menu.menu({
+    select: onMenuSelected,
+  });
 }
 
 function addProgressDialog() {
-    $('body').append(dialog);
-    dialog.append(progressBar);
-    progressBar.progressbar({
-        value: false,
-        change: () => {
-            progressBarLabel.text(progressBar.progressbar('value') + ' / ' + progressBar.progressbar('option', 'max'));
-        }
-    });
-    dialog.dialog({
-        autoOpen: false,
-        buttons: [{
-            class: 'crawler-download-btn',
-            text: 'Download',
-            disabled: true,
-            click: onDownloadClicked
-        }, {
-            class: 'crawler-stop-btn',
-            text: 'Stop',
-            click: onStopClicked
-        }]
-    });
+  $("body").append(dialog);
+  dialog.append(progressBar);
+  progressBar.progressbar({
+    value: false,
+    change: () => {
+      progressBarLabel.text(
+        progressBar.progressbar("value") +
+          " / " +
+          progressBar.progressbar("option", "max")
+      );
+    },
+  });
+  dialog.dialog({
+    autoOpen: false,
+    buttons: [
+      {
+        class: "crawler-download-btn",
+        text: "Download",
+        disabled: true,
+        click: onDownloadClicked,
+      },
+      {
+        class: "crawler-stop-btn",
+        text: "Stop",
+        click: onStopClicked,
+      },
+    ],
+  });
 }
 
 function init() {
-    addStyles();
-    addMenu();
-    addProgressDialog();
-    if ($('meta[property="og:type"]').prop('content') === 'profile') {
-        menu.find('.menu-followers-btn > div').removeClass('ui-state-disabled');
-    }
+  addStyles();
+  addMenu();
+  addProgressDialog();
+  if ($('meta[property="og:type"]').prop("content") === "profile") {
+    menu.find(".menu-followers-btn > div").removeClass("ui-state-disabled");
+  }
 }
 
 function onMenuSelected(event, ui) {
-    console.log('Menu selected!');
-    console.log(event);
-    console.log(ui);
-    if (ui.item.hasClass('menu-followers-btn')) {
-        onFollowerButtonClicked();
-    }
+  console.log("Menu selected!");
+  console.log(event);
+  console.log(ui);
+  if (ui.item.hasClass("menu-followers-btn")) {
+    onFollowerButtonClicked();
+  }
 }
 
 function onStopClicked() {
-    stopped = true;
+  stopped = true;
+  onStopped();
 }
 
 function onDownloadClicked() {
-    downloadFollowers();
-    // FIXME cleanup and close dialog
+  downloadFollowers();
+  // FIXME cleanup and close dialog
 }
 
 function onStopped() {
-    dialog.dialog('widget').find('.crawler-download-btn').button('enable');
+  dialog.dialog("widget").find(".crawler-download-btn").button("enable");
 }
 
 async function onFollowerButtonClicked() {
-    var evt = document.createEvent("MouseEvents");
-    evt.initEvent('click', true, true);
-    authorId = __initialData.data.entry_data.ProfilePage[0].graphql.user.id;
-    $('a[href$="/followers/"]').get(0).dispatchEvent(evt);
-    await sleep(2000);
-    dialog.dialog('open');
+  var evt = document.createEvent("MouseEvents");
+  evt.initEvent("click", true, true);
+  authorId = __initialData.data.entry_data.ProfilePage[0].graphql.user.id;
+  $('a[href$="/followers/"]').get(0).dispatchEvent(evt);
+  await sleep(2000);
+  dialog.dialog("open");
 }
 
 async function scrollToBottom(selector) {
-    await sleep(1000);
-    const scrollable = $(selector);
+  await sleep(1000);
+  const scrollable = $(selector);
 
-    if (scrollable[0].scrollHeight - scrollable[0].scrollTop === scrollable[0].clientHeight) {
-        onStopped();
+  if (
+    scrollable[0].scrollHeight - scrollable[0].scrollTop ===
+    scrollable[0].clientHeight
+  ) {
+    onStopped();
+  } else {
+    if (!stopped) {
+      scrollable.scrollTop(scrollable[0].scrollHeight);
     } else {
-        if (!stopped) {
-            scrollable.scrollTop(scrollable[0].scrollHeight);
-        } else {
-            onStopped();
-        }
+      onStopped();
     }
+  }
 }
 
 function downloadFollowers() {
-    const blob = new Blob([JSON.stringify(followers)], { type: 'text/plain;charset=utf-8' });
-    saveAs(blob, 'followers_' + authorId + '.json');
+  const blob = new Blob([JSON.stringify(followers)], {
+    type: "text/plain;charset=utf-8",
+  });
+  saveAs(blob, "followers_" + authorId + ".json");
 }
 
 (function (open) {
-    XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
-        if (url.indexOf('/followers/') >= 0) {
-            // rewriting url
-            url = url.replace(/count=\d+/, `count=${followersPerPage}`)
-            // const cursor = 'QVFBbXVwT2tMT2R4aWNNWDdaR0ItbWxORWxmME9lMzRCZVhTZTltQ0tlSFhPTXUwQjM3Zll1b2pWem9TYWx2MkpFOHl4QVpqVmJXNGhCdUhIVkVFYmxDRQ%3D%3D';
-            // url = url.replace(new RegExp(`count=${followersPerPage}&search_surface`), `count=${followersPerPage}&max_id=${cursor}&search_surface`);
-            this.addEventListener('readystatechange', () => {
-                if (this.readyState === 4) {
-                    console.log('Response received');
-                    const response = JSON.parse(this.responseText);
-                    // console.log(response);
-                    followers = followers.concat(response.users);
+  XMLHttpRequest.prototype.open = function (method, url, async, user, pass) {
+    if (url.indexOf("/followers/") >= 0) {
+      // rewriting url
+      url = url.replace(/count=\d+/, `count=${followersPerPage}`);
+      // const cursor = 'QVFBbXVwT2tMT2R4aWNNWDdaR0ItbWxORWxmME9lMzRCZVhTZTltQ0tlSFhPTXUwQjM3Zll1b2pWem9TYWx2MkpFOHl4QVpqVmJXNGhCdUhIVkVFYmxDRQ%3D%3D';
+      // url = url.replace(new RegExp(`count=${followersPerPage}&search_surface`), `count=${followersPerPage}&max_id=${cursor}&search_surface`);
+      this.addEventListener(
+        "readystatechange",
+        () => {
+          if (this.readyState === 4) {
+            console.log("Response received");
+            const response = JSON.parse(this.responseText);
+            // console.log(response);
+            followers = followers.concat(response.users);
 
-                    // rewriting response
-                    response.users = followers.slice(-12);
-                    Object.defineProperty(this, 'responseText', {writable: true});
-                    this.responseText = JSON.stringify(response);
+            // rewriting response
+            response.users = followers.slice(-12);
+            Object.defineProperty(this, "responseText", { writable: true });
+            this.responseText = JSON.stringify(response);
 
-                    progressBar.progressbar({
-                        max: Math.min(__initialData.data.entry_data.ProfilePage[0].graphql.user.edge_followed_by.count, maxFollowersPerAccount),
-                        value: followers.length
-                    });
-                    if (followers.length > maxFollowersPerAccount) {
-                        onStopped();
-                    } else {
-                        scrollToBottom('div[aria-label="Followers"] .isgrP');
-                    }
-                }
-            }, false);
-        }
-        open.call(this, method, url, async, user, pass);
-    };
+            progressBar.progressbar({
+              max: Math.min(
+                __initialData.data.entry_data.ProfilePage[0].graphql.user
+                  .edge_followed_by.count,
+                maxFollowersPerAccount
+              ),
+              value: followers.length,
+            });
+            if (followers.length > maxFollowersPerAccount) {
+              onStopped();
+            } else {
+              scrollToBottom('div[aria-label="Followers"] .isgrP');
+            }
+          }
+        },
+        false
+      );
+    }
+    open.call(this, method, url, async, user, pass);
+  };
 })(XMLHttpRequest.prototype.open);
 
 function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 (function () {
-    'use strict';
+  "use strict";
 
-    waitForKeyElements('article', init, true);
-
+  waitForKeyElements("article", init, true);
 })();
