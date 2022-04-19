8e2ec5e28b88081311e0fddd4cf9d3531b9b01c2
umuero <umut.erogul@somera.com.tr>
2022-04-19 22:06:45 +0300

    v2 wrapper initial

--- straders/v2.py
+++ straders/v2.py
@@ -1,10 +1,11 @@
+import argparse
 import logging
 import json
 import time
 import math
+from dataclasses import asdict
 from dacite import from_dict
-from v2_types import *
-import v2_calls
+from v2base import *
 
 
 USERNAME = "umuero"
@@ -20,40 +21,26 @@ def loadJson(fileName: str) -> State:
         with open(fileName, "r") as f:
             loadJson = json.load(f)
             return from_dict(State, loadJson)
-    except Exception:
-        print("no save data, continuing")
-    return State()
+    except Exception as e:
+        logger.exception("no save data, continuing")
+    return None
 
 
 def saveJson(fileName: str, st: State):
     with open(fileName, "w") as f:
-        f.write(st.toJSON())
-
-
-############ RESET LOAN ############
-"""
-documentations
-ShipNavigation.arrivedAt 
-
-deliver_on_contract
-js.data.data
-"""
-
-
-def reset():
-    token = ""
-    while token == "":
-        st = v2_calls.systems(USERNAME)
-        if st.token:
-            v2_calls.TOKEN = st.token
-            token = st.token
-        else:
-            time.sleep(60)
-            continue
-        st.systems = v2_calls.list_systems()
+        json.dump(asdict(st), f, indent=2)
 
 
 """
+    def distance(self, locationA: Location, locationB: Location):
+        dx = abs(locationA.x-locationB.x)
+        dy = abs(locationA.y-locationB.y)
+        sq = sqrt(dx*dx+dy*dy)
+        return round(sq)
+
+    def flightTime(self, a: Location, b: Location, speed=1):
+        return round(self.distance(a, b)*(3/speed)+30)
+        
         availableLoans()
         availableGoods()
         availableShips()
@@ -87,4 +74,146 @@ def updateGalaxy(galaxy="OE"):
                     if newW not in st.galaxies[g]:
                         st.galaxies[g][newW] = st.galaxies[g][currentG].copy()
                         st.galaxies[g][newW].append(warp["symbol"])
+"""
+
+
+def main():
+    parser = argparse.ArgumentParser(description="SpaceTradersV2")
+    parser.add_argument("-i", "--info", help="print json and exit", action="store_true")
+    parser.add_argument("-x", "--extra", help="print extra info and exit", action="store_true")
+    parser.add_argument("-l", "--leader", help="print leaders and exit", action="store_true")
+    parser.add_argument("-s", "--sleep", help="sleep seconds", default=60, type=int)
+    parser.add_argument("--username", help="set username")
+    args = parser.parse_args()
+    if args.username:
+        USERNAME = args.username
+
+    st = loadJson(SAVE)
+    if st is None:
+        v2 = V2("")
+        st = v2.agent_register(USERNAME)
+        v2.token = st.token
+    v2 = V2(st.token)
+
+    if args.info:
+        logger.info(asdict(st.agent))
+        logger.info("== SHIPS ==")
+        for ship in st.ships:
+            logger.info(asdict(ship))
+        logger.info("== CONTRACTS ==")
+        for contract in st.contracts:
+            logger.info(asdict(contract))
+        if args.extra:
+            logger.info("== SYSTEMS ==")
+            for system in st.systems.values():
+                logger.info(asdict(system))
+        return
+
+    if args.leader:
+        v2.leaderboard()
+        return
+
+    loopCtr = 1
+    while loopCtr > 0:
+        try:
+            st.agent = v2.my_agent()
+            st.contracts = v2.contract_list()
+            st.ships = v2.ship_list()
+            if loopCtr % 10 == 0:
+                # daha seyrek cekmek lazim
+                st.systems = {s.symbol: s for s in v2.system_list()}
+                st.systems = {s.symbol: s for s in v2.system_list()}
+
+            # X1-OE de gez, survey extract -> sat ??
+            # her navigate result -> st.cooldowns.append
+            # her survey extract scan result -> st.cooldowns.append
+
+            # sellAndDeposit()
+            # decideExpansion()
+            # trades = findTrades()
+            # decideTrades(trades)
+            # executeOrders()
+            saveJson(SAVE, st)
+        except Exception as e:
+            logger.exception("main loop failed")
+
+        loopCtr += 1
+        print("sleeping", args.sleep)
+        time.sleep(args.sleep)
+
+
+if __name__ == "__main__":
+    main()
+
+
+"""
+In [70]: st.ships[0]
+Out[70]: Ship(symbol='UMUERO-1', frame='FRAME_FRIGATE', reactor='REACTOR_FUSION_I', engine='ENGINE_ION_DRIVE_II', fuel=1200, modules=['MODULE_GAS_TANK', 'MODULE_CARGO_HOLD', 'MODULE_CARGO_HOLD', 'MODULE_CREW_QUARTERS', 'MODULE_ENVOY_QUARTERS', 'MODULE_JUMP_DRIVE_I'], mounts=['MOUNT_SENSOR_ARRAY_II', 'MOUNT_MINING_LASER_II', 'MOUNT_GAS_SIPHON_II'], registration=Registration(factionSymbol='COMMERCE_REPUBLIC', agentSymbol='UMUERO', fee=0, role='COMMAND'), integrity=Integrity(frame=1, reactor=1, engine=1), stats=ShipStats(fuelTank=1200, cargoLimit=280, jumpRange=20), status='DOCKED', location='X1-OE-PM', cargo=[])
+
+In [71]: st.contracts[0]
+Out[71]: Contract(id='cl263cjvy001401s6alc25vab', faction='COMMERCE_REPUBLIC', type='PROCUREMENT', terms=Term(deadline='2022-05-03T11:57:12.235Z', payment=Payment(onAccepted=30000, onFulfilled=120001), deliver=[ContractDelivery(tradeSymbol='IRON_ORE', destination='X1-OE-PM', units=500, fulfilled=0)]), accepted=False, fulfilled=False, expiresAt='2022-04-26T11:57:12.233Z')
+
+
+v2_calls.TOKEN = st.token
+
+v2_calls.list_contracts()
+2022-04-19 16:21:00	INFO	v2.calls: list_contracts: {'total': 1, 'page': 1, 'limit': 20}
+Out[51]: [Contract(id='cl263cjvy001401s6alc25vab', faction='COMMERCE_REPUBLIC', type='PROCUREMENT', terms=Term(deadline='2022-05-03T11:57:12.235Z', payment=Payment(onAccepted=30000, onFulfilled=120001), deliver=[ContractDelivery(tradeSymbol='IRON_ORE', destination='X1-OE-PM', units=500, fulfilled=0)]), accepted=True, fulfilled=False, expiresAt='2022-04-26T11:57:12.233Z')]
+
+In [53]: v2_calls.my_agent()
+2022-04-19 16:21:35	INFO	v2.calls: my_agent: {'data': {'accountId': 'cl263cjpz000301s6h1e5uxju', 'symbol': 'UMUERO', 'headquarters': 'X1-OE-PM', 'credits': 130000}}
+Out[53]: Agent(accountId='cl263cjpz000301s6h1e5uxju', symbol='UMUERO', headquarters='X1-OE-PM', credits=130000)
+
+In [54]: v2_calls.my_account()
+2022-04-19 16:21:41	INFO	v2.calls: my_account: {'data': {'account': {'id': 'cl263cjpz000301s6h1e5uxju', 'email': 'temp-account-2e6c1483-275b-4008-a416-77f03a92995d@spacetraders.io', 'discordHandle': None, 'patreonId': None, 'createdAt': '2022-04-19T11:57:12.023Z'}}}
+Out[54]:
+{'data': {'account': {'id': 'cl263cjpz000301s6h1e5uxju',
+   'email': 'temp-account-2e6c1483-275b-4008-a416-77f03a92995d@spacetraders.io',
+   'discordHandle': None,
+   'patreonId': None,
+   'createdAt': '2022-04-19T11:57:12.023Z'}}}
+
+
+In [55]: v2_calls.view_exports('IRON_ORE')
+2022-04-19 16:22:23	INFO	v2.calls: view_exports: {'total': 0, 'page': 1, 'limit': 20}
+Out[55]: []
+
+In [56]: v2_calls.view_imports('IRON_ORE')
+2022-04-19 16:22:34	INFO	v2.calls: view_imports: {'total': 1, 'page': 1, 'limit': 20}
+Out[56]: [MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='IRON_ORE', price=388, tariff=0)]
+
+
+In [85]: v2_calls.list_markets('X1-OE')
+2022-04-19 16:34:22	INFO	v2.calls: list_markets: {'total': 3, 'page': 1, 'limit': 20}
+Out[85]: ['X1-OE-PM', 'X1-OE-PM01', 'X1-OE-PM02']
+
+In [20]: v2.navigate_ship(st.ships[0].symbol, 'X1-OE-PM01')
+2022-04-19 21:15:06	INFO	v2c: navigate_ship {'fuelCost': 0, 'navigation': {'shipSymbol': 'UMUERO-1', 'departure': 'X1-OE-PM', 'destination': 'X1-OE-PM01', 'durationRemaining': 0, 'arrivedAt': '2022-04-19T18:15:06.989Z'}}
+Out[20]: ShipNavigation(shipSymbol='UMUERO-1', departure='X1-OE-PM', destination='X1-OE-PM01', durationRemaining=0, arrivedAt='2022-04-19T18:15:06.989Z')
+
+In [32]: v2.navigate_ship(st.ships[0].symbol, 'X1-OE-A005')
+2022-04-19 21:20:26	INFO	v2c: navigate_ship {'fuelCost': 28, 'navigation': {'shipSymbol': 'UMUERO-1', 'departure': 'X1-OE-PM02', 'destination': 'X1-OE-A005', 'durationRemaining': 55, 'arrivedAt': None}}
+Out[32]: ShipNavigation(shipSymbol='UMUERO-1', departure='X1-OE-PM02', destination='X1-OE-A005', durationRemaining=55, arrivedAt=None)
+
+In [28]: v2.view_market('X1-OE', 'X1-OE-PM01')
+Out[28]: MarketListing(exports=[MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='IRON', price=331, tariff=0), MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='ALUMINUM', price=371, tariff=0), MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='COPPER', price=373, tariff=0)], imports=[MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='IRON_ORE', price=386, tariff=0), MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='ALUMINUM_ORE', price=314, tariff=0), MarketTrade(waypointSymbol='X1-OE-PM01', tradeSymbol='COPPER_ORE', price=329, tariff=0)], exchange=[])
+
+In [29]: v2.view_market('X1-OE', 'X1-OE-PM02')
+Out[29]: MarketListing(exports=[MarketTrade(waypointSymbol='X1-OE-PM02', tradeSymbol='COMM_RELAY_I', price=28376, tariff=0)], imports=[MarketTrade(waypointSymbol='X1-OE-PM02', tradeSymbol='ALUMINUM', price=388, tariff=0), MarketTrade(waypointSymbol='X1-OE-PM02', tradeSymbol='ELECTRONICS', price=785, tariff=0)], exchange=[])
+
+
+In [37]: v2.survey_waypoint(st.ships[0].symbol, None)
+2022-04-19 21:26:16	INFO	v2c: survey_waypoint Cooldown(duration=899, expiration='2022-04-19T18:41:16.694Z')
+Out[37]:
+[Survey(signature='X1-OE-70790D', deposits=['ALUMINUM_ORE', 'COPPER_ORE', 'QUARTZ', 'SILICON'], expiration='2022-04-19T18:36:39.697Z'),
+ Survey(signature='X1-OE-43FEC9', deposits=['COPPER_ORE', 'IRON_ORE'], expiration='2022-04-19T18:52:55.697Z'),
+ Survey(signature='X1-OE-F46A3B', deposits=['COPPER_ORE', 'QUARTZ'], expiration='2022-04-19T18:50:59.697Z'),
+ Survey(signature='X1-OE-10CCC4', deposits=['QUARTZ'], expiration='2022-04-19T18:43:39.697Z'),
+ Survey(signature='X1-OE-D62E44', deposits=['ALUMINUM_ORE'], expiration='2022-04-19T18:50:58.697Z'),
+ Survey(signature='X1-OE-1803C6', deposits=['ALUMINUM_ORE', 'SILICON'], expiration='2022-04-19T18:49:00.698Z'),
+ Survey(signature='X1-OE-C0F1EC', deposits=['ALUMINUM_ORE', 'ALUMINUM_ORE', 'COPPER_ORE', 'QUARTZ'], expiration='2022-04-19T18:33:22.698Z'),
+ Survey(signature='X1-OE-4EBBD4', deposits=['ALUMINUM_ORE', 'COPPER_ORE', 'COPPER_ORE'], expiration='2022-04-19T18:32:08.698Z'),
+ Survey(signature='X1-OE-2D7A68', deposits=['COPPER_ORE'], expiration='2022-04-19T18:43:08.698Z')]
+
+
 """
---
+++ straders/v2base.py
@@ -0,0 +1,489 @@
+import json
+import logging
+from dataclasses import dataclass
+from typing import Optional, Union
+
+import ratelimit
+import requests
+from dacite import from_dict
+
+logger = logging.getLogger("v2c")
+logging.basicConfig(level=logging.INFO, format="%(asctime)s\t%(levelname)s\t%(name)s: %(message)s", datefmt="%Y-%m-%d %H:%M:%S")
+
+
+@dataclass
+class Agent:
+    accountId: str
+    symbol: str
+    headquarters: str
+    credits: int
+
+
+@dataclass
+class Faction:
+    symbol: str
+    name: str
+    description: str
+    headquarters: str
+    traits: list[str]
+
+
+@dataclass
+class Payment:
+    onAccepted: int
+    onFulfilled: int
+
+
+@dataclass
+class ContractDelivery:
+    tradeSymbol: str
+    destination: str
+    units: int
+    fulfilled: int
+
+
+@dataclass
+class Term:
+    deadline: str  # "2022-03-09T05:16:59.112Z"
+    payment: Payment
+    deliver: list[ContractDelivery]
+
+
+@dataclass
+class Contract:
+    id: str
+    faction: str
+    type: str
+    terms: Term
+    accepted: bool
+    fulfilled: bool
+    expiresAt: str  # "2022-03-09T05:16:59.112Z"
+
+
+@dataclass
+class Survey:
+    signature: str
+    deposits: list[str]
+    expiration: str  # "2022-03-09T05:16:59.112Z"
+
+
+@dataclass
+class Registration:
+    factionSymbol: str
+    agentSymbol: str
+    fee: int
+    role: str
+
+
+@dataclass
+class Integrity:
+    frame: int
+    reactor: int
+    engine: int
+
+
+@dataclass
+class Good:
+    tradeSymbol: str
+    units: int
+
+
+@dataclass
+class ShipStats:
+    fuelTank: int
+    cargoLimit: int
+    jumpRange: int
+
+
+@dataclass
+class Cooldown:
+    duration: int
+    expiration: str
+
+
+@dataclass
+class Extraction:
+    shipSymbol: str
+    yields: Good
+
+
+@dataclass
+class Ship:
+    symbol: str
+    frame: str
+    reactor: str
+    engine: str
+    fuel: int
+    engine: str
+    modules: list[str]
+    mounts: list[str]
+    registration: Registration
+    integrity: Integrity
+    stats: Optional[ShipStats]
+    status: str
+    location: str
+    cargo: list[Good]
+
+
+@dataclass
+class Jump:
+    shipSymbol: str
+    destination: str
+
+
+@dataclass
+class ShipNavigation:
+    shipSymbol: str
+    departure: str
+    destination: str
+    durationRemaining: int
+    arrivedAt: Optional[str]
+
+
+@dataclass
+class MarketTrade:
+    waypointSymbol: str
+    tradeSymbol: str
+    price: int
+    tariff: int
+
+
+@dataclass
+class Trade:
+    waypointSymbol: str
+    tradeSymbol: str
+    credits: int
+    units: int
+
+
+@dataclass
+class MarketListing:
+    exports: list[MarketTrade]
+    imports: list[MarketTrade]
+    exchange: list[MarketTrade]
+
+
+@dataclass
+class System:
+    symbol: str
+    sector: str
+    type: str
+    x: int
+    y: int
+    waypoints: list[str]
+    factions: list[Optional[str]]
+    charted: bool
+    chartedBy: Optional[str]
+
+
+@dataclass
+class Waypoint:
+    system: str
+    symbol: str
+    type: str
+    x: int
+    y: int
+    orbitals: list[str]
+    faction: str
+    features: list[str]
+    traits: list[str]
+    charted: bool
+    chartedBy: Optional[str]
+
+
+@dataclass
+class Shipyard:
+    symbol: str
+    faction: str
+
+
+@dataclass
+class ShipyardListing:
+    id: str
+    waypoint: str
+    price: int
+    role: str
+    frame: str
+    reactor: str
+    engine: str
+    engine: str
+    modules: list[str]
+    mounts: list[str]
+
+
+@dataclass
+class Meta:
+    total: int
+    page: int
+    limit: int
+
+
+@dataclass
+class State:
+    token: str
+    agent: Agent
+    faction: Faction
+    contracts: list[Contract]
+    ships: list[Ship]
+    systems: dict[str, System]
+    markets: dict[str, Optional[MarketListing]]
+    # 'X1-OE-PM:X1-OE-PM01': 0 # fuel || time
+    # 'X1-OE-PM02:X1-OE-A005': 28 fuel, 55 time # fuel || time
+    distances: dict[str, int]
+    # last market update @ X1-OE-PM: epoch ?? (hersey isoformat time olsun mu ??)
+    updates: dict[str, int]
+    cooldowns: list[ShipNavigation]  # cooldownlari da buna cevireyim, gezip kontrol edem
+
+
+class V2:
+    host = "https://v2-0-0.alpha.spacetraders.io/"
+    token = ""
+    timeout = 60
+    session: requests.Session = None
+
+    def __init__(self, token: str):
+        self.token = token
+        self.session = requests.session()
+        self.session.headers.update({"Authorization": "Bearer " + self.token})
+
+    @ratelimit.sleep_and_retry
+    @ratelimit.limits(calls=2, period=1)
+    def callApi(self, method: str, url: str, data={}, noToken=False):
+        # Each endpoint also has an example response you can retrieve by sending stub=true as a query parameter or in your POST body.
+        # You can also view a stubbed response for an error code by sending errorCode={code} with the stub parameter.
+        if method == "GET":
+            js = self.session.get(self.host + url, params=data, timeout=self.timeout).json()
+        if method == "POST":
+            js = self.session.post(self.host + url, json=data, timeout=self.timeout).json()
+        if "error" in js:
+            logger.info("FAIL: %s %s %s %s", method, url, data, js)
+        return js
+
+    ################# LOGIN #################
+    def leaderboard(self):
+        js = self.callApi("GET", "")
+        logger.info(json.dumps(js["stats"]))
+        logger.info("mostCredits")
+        for node in js.get("leaderboards", {}).get("mostCredits", []):
+            logger.info(f"{node['agentSymbol']:>10}: {node['credits']:>12,}")
+        logger.info("mostSubmittedCharts")
+        for node in js.get("leaderboards", {}).get("mostSubmittedCharts", []):
+            logger.info(f"{node['agentSymbol']:>10}: {node['chartCount']:>12,}")
+
+    def agent_register(self, username: str, faction: str = "COMMERCE_REPUBLIC") -> State:
+        js = self.callApi("POST", "agents", {"symbol": username, "faction": faction}, noToken=True)
+        logger.info("agent_register: %s %s %s", username, faction, js)
+        js["data"]["contracts"] = [js["data"]["contract"]]
+        js["data"]["ships"] = [js["data"]["ship"]]
+        js["data"]["systems"] = {s.symbol: s for s in self.system_list()}
+        js["data"]["markets"] = []
+        st = from_dict(State, js["data"])
+        return st
+
+    def my_agent(self) -> Agent:
+        js = self.callApi("GET", "my/agent")
+        logger.info("my_agent: %s", js)
+        return from_dict(Agent, js["data"])
+
+    def my_account(self):
+        js = self.callApi("GET", "my/account")
+        logger.info("my_account: %s", js)
+        return js
+
+    ################# SYSTEMS #################
+    def system_list(self, page: int = 1, limit: int = 200) -> list[System]:
+        js = self.callApi("GET", "systems", {"page": page, "limit": limit})
+        logger.info("system_list: %s", js["meta"])
+        return [from_dict(System, j) for j in js["data"]]
+
+    def system_view(self, system_symbol: str) -> System:
+        js = self.callApi("GET", f"systems/{system_symbol}")
+        logger.info("system_view: %s", js)
+        return from_dict(System, js["data"])
+
+    def waypoint_list(self, system_symbol: str) -> list[Waypoint]:
+        js = self.callApi("GET", f"systems/{system_symbol}/waypoints")
+        logger.info("waypoint_list: %s", js["meta"])
+        return [from_dict(Waypoint, j) for j in js["data"]]
+
+    def waypoint_view(self, system_symbol: str, waypoint_symbol: str) -> Waypoint:
+        js = self.callApi("GET", f"systems/{system_symbol}/waypoints/{waypoint_symbol}")
+        return from_dict(Waypoint, js["data"])
+
+    ################# SHIPYARD #################
+    def shipyard_list(self, system_symbol: str) -> list[Shipyard]:
+        js = self.callApi("GET", f"systems/{system_symbol}/shipyards")
+        logger.info("shipyard_list: %s", js["meta"])
+        return [from_dict(Shipyard, j) for j in js["data"]]
+
+    def shipyard_view(self, system_symbol: str, waypoint_symbol: str) -> Shipyard:
+        js = self.callApi("GET", f"systems/{system_symbol}/shipyards/{waypoint_symbol}")
+        return from_dict(Shipyard, js["data"])
+
+    def shipyard_listing(self, system_symbol: str, waypoint_symbol: str) -> list[ShipyardListing]:
+        js = self.callApi("GET", f"systems/{system_symbol}/shipyards/{waypoint_symbol}/ships")
+        logger.info("shipyard_listing: %s", js["meta"])
+        return [from_dict(ShipyardListing, j) for j in js["data"]]
+
+    def purchase_ship(self, id: str) -> list[str]:
+        js = self.callApi("POST", f"my/ships", {id: id})
+        logger.info("purchase_ship: %d", js["data"]["credits"])
+        return from_dict(Ship, js["data"]["ship"])
+
+    ################# MARKET #################
+    def market_list(self, system_symbol: str) -> list[str]:
+        js = self.callApi("GET", f"systems/{system_symbol}/markets")
+        logger.info("market_list: %s", js["meta"])
+        return js["data"]
+
+    def market_view(self, system_symbol: str, waypoint_symbol: str) -> MarketListing:
+        js = self.callApi("GET", f"systems/{system_symbol}/markets/{waypoint_symbol}")
+        return from_dict(MarketListing, js["data"])
+
+    def market_imports(self, trade_symbol: str) -> list[MarketTrade]:
+        js = self.callApi("GET", f"trade/{trade_symbol}/imports")
+        logger.info("market_imports: %s", js["meta"])
+        return [from_dict(MarketTrade, j) for j in js["data"]]
+
+    def market_exports(self, trade_symbol: str) -> list[MarketTrade]:
+        js = self.callApi("GET", f"trade/{trade_symbol}/exports")
+        logger.info("market_exports: %s", js["meta"])
+        return [from_dict(MarketTrade, j) for j in js["data"]]
+
+    def market_exchange(self, trade_symbol: str) -> list[MarketTrade]:
+        js = self.callApi("GET", f"trade/{trade_symbol}/exchange")
+        logger.info("market_exchange: %s", js["meta"])
+        return [from_dict(MarketTrade, j) for j in js["data"]]
+
+    ################# CONTRACT #################
+    def contract_list(self) -> list[Contract]:
+        js = self.callApi("GET", "my/contracts")
+        logger.info("list_contracts: %s", js["meta"])
+        return [from_dict(Contract, j) for j in js["data"]]
+
+    def contract_view(self, contract_id) -> Contract:
+        js = self.callApi("GET", f"my/contracts/{contract_id}")
+        return from_dict(Contract, js["data"])
+
+    def contract_accept(self, contract_id) -> Contract:
+        js = self.callApi("POST", f"my/contracts/{contract_id}/accept")
+        return from_dict(Contract, js["data"])
+
+    ################# SHIP #################
+    def ship_list(self) -> list[Ship]:
+        js = self.callApi("GET", f"my/ships", {"limit": 100})
+        logger.info("list_ships: %s", js["meta"])
+        return [from_dict(Ship, j) for j in js["data"]]
+
+    def ship_view(self, ship_symbol: str) -> Ship:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}")
+        return from_dict(Ship, js["data"])
+
+    def ship_cooldown_scan(self, ship_symbol: str) -> Cooldown:
+        js = self.callApi("GET", f"my/ships/{ship_symbol}/scan")
+        return from_dict(Cooldown, js["data"]["cooldown"])
+
+    def ship_cooldown_extract(self, ship_symbol: str) -> Cooldown:
+        js = self.callApi("GET", f"my/ships/{ship_symbol}/extract")
+        return from_dict(Cooldown, js["data"]["cooldown"])
+
+    def ship_cooldown_survey(self, ship_symbol: str) -> Cooldown:
+        js = self.callApi("GET", f"my/ships/{ship_symbol}/survey")
+        return from_dict(Cooldown, js["data"]["cooldown"])
+
+    def ship_cooldown_jump(self, ship_symbol: str) -> Cooldown:
+        js = self.callApi("GET", f"my/ships/{ship_symbol}/jump")
+        return from_dict(Cooldown, js["data"]["cooldown"])
+
+    def ship_status_navigation(self, ship_symbol: str) -> ShipNavigation:
+        js = self.callApi("GET", f"my/ships/{ship_symbol}/navigate")
+        return from_dict(ShipNavigation, js["data"]["navigation"])
+
+    ################# NAVIGATION #################
+    def ship_refuel(self, ship_symbol: str):
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/refuel")
+        logger.info("refuel_ship: %s", js)
+        return js["data"]
+
+    def ship_dock(self, ship_symbol: str) -> str:
+        # mode=STEALTH
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/dock")
+        logger.info("dock_ship %s", js)
+        return js["data"]["status"]
+
+    def ship_orbit(self, ship_symbol: str) -> str:
+        # mode=BLOCKADE,SPY,PATROL
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/orbit")
+        logger.info("orbit_ship %s", js)
+        return js["data"]["status"]
+
+    def ship_jump(self, ship_symbol: str, destination: str) -> Jump:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/jump", {"destination": destination})
+        cooldown = from_dict(Cooldown, js["data"]["cooldown"])
+        logger.info("jump_ship %s", cooldown)
+        return from_dict(Jump, js["data"]["jump"])
+
+    def ship_navigate(self, ship_symbol: str, destination: str) -> ShipNavigation:
+        # mode=CRUISE ...
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/navigate", {"destination": destination})
+        logger.info("navigate_ship %s", js["data"])
+        return from_dict(ShipNavigation, js["data"]["navigation"])
+
+    ################# SHIP ACTIONS #################
+    def chart_waypoint(self, ship_symbol: str) -> list[str]:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/chart")
+        logger.info("chart_waypoint: %s", js)
+        return js["data"]
+
+    def deploy_asset(self, ship_symbol: str) -> str:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/deploy")
+        logger.info("deploy_asset: %s", js)
+        return js
+
+    def activate_scan(self, ship_symbol: str, mode: str) -> Union[Waypoint, System, list[Ship]]:
+        # mode: APPROACHING_SHIPS DEPARTING_SHIPS SYSTEM WAYPOINT
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/scan", {"mode": mode})
+        cooldown = from_dict(Cooldown, js["data"]["cooldown"])
+        logger.info("activate_scan %s", cooldown)
+        if "waypoint" in js["data"]:
+            return from_dict(Waypoint, js["data"]["waypoint"])
+        if "system" in js["data"]:
+            return from_dict(System, js["data"]["system"])
+        if "ships" in js["data"]:
+            return [from_dict(Cooldown, j) for j in js["data"]["ships"]]
+
+    def extract_resources(self, ship_symbol: str, survey: Optional[Survey]) -> Extraction:
+        if survey:
+            js = self.callApi("POST", f"my/ships/{ship_symbol}/extract", {"survey": survey})
+        else:
+            js = self.callApi("POST", f"my/ships/{ship_symbol}/extract")  # random extraction
+        cooldown = from_dict(Cooldown, js["data"]["cooldown"])
+        logger.info("extract_resources %s", cooldown)
+        js["data"]["extraction"]["yields"] = js["data"]["extraction"]["yield"]
+        return from_dict(Extraction, js["data"]["extraction"])
+
+    def survey_waypoint(self, ship_symbol: str, survey: Optional[Survey]) -> list[Survey]:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/survey", {"survey": survey})
+        cooldown = from_dict(Cooldown, js["data"]["cooldown"])
+        logger.info("survey_waypoint %s", cooldown)
+        return [from_dict(Survey, j) for j in js["data"]["surveys"]]
+
+    ################# SHIP CARGO ACTIONS #################
+    def jettison_cargo(self, ship_symbol: str, trade_symbol: str, units: int) -> Good:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/jettison", {"tradeSymbol": trade_symbol, "units": units})
+        return from_dict(Good, js["data"])
+
+    def contract_deliver(self, contract_id: str, ship_symbol: str, trade_symbol: str, units: int) -> ContractDelivery:
+        js = self.callApi(
+            "POST", f"my/ships/{ship_symbol}/deliver", {"contractId": contract_id, "tradeSymbol": trade_symbol, "units": units}
+        )
+        logger.info("deliver_contract %s", js)
+        return from_dict(ContractDelivery, js["data"])  # ["data"]["data"] ??
+
+    def purchase_cargo(self, ship_symbol: str, trade_symbol: str, units: int) -> Trade:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/purchase", {"tradeSymbol": trade_symbol, "units": units})
+        return from_dict(Trade, js["data"])
+
+    def sell_cargo(self, ship_symbol: str, trade_symbol: str, units: int) -> Trade:
+        js = self.callApi("POST", f"my/ships/{ship_symbol}/sell", {"tradeSymbol": trade_symbol, "units": units})
+        return from_dict(Trade, js["data"])
